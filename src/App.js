2559e33911973efc200f3e98b3ebbc2cecc6e617
Roba Nath Basnet <robanbth@gmail.com>
2022-11-18 16:22:31 +0600

    User Managed via State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
 function App() {
+	const [userList, setUserList] = useState([]);
+	const addUserHandler = (uName, uAge) => {
+		setUserList((prevUserList) => {
+			return [
+				...prevUserList,
+				{ name: uName, age: uAge, id: Math.random().toString() },
+			];
+		});
+	};
 	return (
 		<div>
-			<AddUser />
-			<UserList users={[]} />
+			<AddUser onAddUser={addUserHandler} />
+			<UserList users={userList} />
 		</div>
 	);
 }
